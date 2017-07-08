9a4964a2fc6a62718364837fb623ebede43614e0
= <=>
2017-07-09 00:49:50 +0530

    git commit -m "like_user_post modified"

--- like_user_post.py
+++ like_user_post.py
@@ -1,18 +1,19 @@
-  import requests
+  import requests,urllib
   from constants import APP_ACESS_TOKEN,BASE_URL
   from get_users_post import get_users_post
+  from get_user_id import get_user_id
 
-  username = "saloni2408"
-  def like_a_post(insta_username):
-    post_id =get_user_id(insta_username)
-  # print media_id
-    request_url = (BASE_URL + '','media/%s/likes') % (post_id)
-  payload = {"access_token": APP_ACESS_TOKEN}
-  print 'POST request url : %s' % (request_url)
-  post_a_like = requests.post(request_url, payload).json()
+  username = "yogesh_biebz"
+    def like_a_post(insta_username):
+      post_id =get_user_id(insta_username)
+    # print media_id
+      request_url = (BASE_URL + '','media/%s/likes') % (post_id)
+    payload = {"access_token": APP_ACESS_TOKEN}
+    print 'POST request url : %s' % (request_url)
+    post_a_like = requests.post(request_url, payload).json()
 
-  if post_a_like['meta']['code'] == 200:
-    print "Post Liked Successfully"
-  else:
-    print "Unable to like post"
-like_a_post(username)
+    if post_a_like['meta']['code'] == 200:
+      print "Post Liked Successfully"
+    else:
+      print "Unable to like post"
+  like_a_post(username)
